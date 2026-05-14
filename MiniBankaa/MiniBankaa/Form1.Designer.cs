df6b06c42d4de49afa62c0138e5cda2565dea553
Meliisaylmzz <melisamelekylmz1656@gmail.com>
2026-05-14 22:20:32 +0300

    Add files via upload

---
+++ MiniBankaa/MiniBankaa/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MiniBankaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnYatir = new System.Windows.Forms.Button();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.lstIslemler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(168, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MINI BANKA SISTEMI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBakiye.Location = new System.Drawing.Point(147, 88);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(118, 20);
+            this.lblBakiye.TabIndex = 1;
+            this.lblBakiye.Text = "Bakiye: 1000TL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(147, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tutar Giriniz:";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(251, 124);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 20);
+            this.txtTutar.TabIndex = 3;
+            // 
+            // btnYatir
+            // 
+            this.btnYatir.Location = new System.Drawing.Point(151, 179);
+            this.btnYatir.Name = "btnYatir";
+            this.btnYatir.Size = new System.Drawing.Size(75, 23);
+            this.btnYatir.TabIndex = 4;
+            this.btnYatir.Text = "Para Yatır";
+            this.btnYatir.UseVisualStyleBackColor = true;
+            this.btnYatir.Click += new System.EventHandler(this.btnYatir_Click);
+            // 
+            // btnCek
+            // 
+            this.btnCek.Location = new System.Drawing.Point(151, 217);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(75, 23);
+            this.btnCek.TabIndex = 5;
+            this.btnCek.Text = "Para Çek";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(151, 255);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(94, 23);
+            this.btnGoster.TabIndex = 6;
+            this.btnGoster.Text = "Bakiye Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(151, 293);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(75, 23);
+            this.btnCikis.TabIndex = 7;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // lstIslemler
+            // 
+            this.lstIslemler.FormattingEnabled = true;
+            this.lstIslemler.Location = new System.Drawing.Point(141, 340);
+            this.lstIslemler.Name = "lstIslemler";
+            this.lstIslemler.Size = new System.Drawing.Size(143, 82);
+            this.lstIslemler.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstIslemler);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.btnCek);
+            this.Controls.Add(this.btnYatir);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Banka Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnYatir;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.ListBox lstIslemler;
+    }
+}
+
---
+++ MiniBankaa/MiniBankaa/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniBankaa
+{
+    public partial class Form1: Form
+    {
+        double bakiye = 1000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYatir_Click(object sender, EventArgs e)
+        {
+           
+        {
+            try
+            {
+                double tutar = Convert.ToDouble(txtTutar.Text);
+
+                if (tutar <= 0)
+                {
+                    MessageBox.Show("Lütfen pozitif bir tutar giriniz.");
+                    return;
+                }
+
+                bakiye += tutar;
+                    lstIslemler.Items.Add("Para Yatırıldı: +" + tutar + " TL");
+                    lblBakiye.Text = "Bakiye: " + bakiye + " TL";
+                txtTutar.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen sadece sayı giriniz.");
+            }
+        }
+        }
+
+        private void btnCek_Click(object sender, EventArgs e)
+        {
+          
+        {
+            try
+            {
+                double tutar = Convert.ToDouble(txtTutar.Text);
+
+                if (tutar <= 0)
+                {
+                    MessageBox.Show("Lütfen pozitif bir tutar giriniz.");
+                    return;
+                }
+
+                if (tutar > bakiye)
+                {
+                    MessageBox.Show("Yetersiz bakiye!");
+                    return;
+                }
+
+                bakiye -= tutar;
+
+                lblBakiye.Text = "Bakiye: " + bakiye + " TL";
+                txtTutar.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen sadece sayı giriniz.");
+            }
+        }
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            lblBakiye.Text = "Bakiye: " + bakiye + " TL";
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
